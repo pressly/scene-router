71490e41df2bd183506c61609e7656c325a47a9c
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-10-29 14:12:45 -0400

    added simple example

--- examples/ExampleWith2SideMenus/index.ios.js
+++ examples/ExampleWith2SideMenus/index.ios.js
@@ -1,53 +1,139 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
+var TwoSideMenus = require('react-native-2-side-menus');
+var Scene = require('scene-router');
+
 var {
+  Component,
   AppRegistry,
   StyleSheet,
   Text,
   View,
 } = React;
 
-var ExampleWith2SideMenus = React.createClass({
-  render: function() {
+async function wait(delay) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+class Content extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <View style={{ flex: 1, backgroundColor: this.props.color, justifyContent: 'center', alignItems: 'center' }}/>
     );
   }
-});
+}
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-AppRegistry.registerComponent('ExampleWith2SideMenus', () => ExampleWith2SideMenus);
+  menuMightOpen() {
+    console.log('menu might open');
+  }
+
+  menuDidOpen() {
+    console.log('menu did open');
+  }
+
+  menuDidClose() {
+    console.log('menu did close');
+  }
+
+  render() {
+    return (
+      <Content {...this.props}/>
+    );
+  }
+}
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <Content color="blue"/>
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const { menu, leftMenu, rightMenu } = this.refs;
+    menu.moreLifeCycles(leftMenu, rightMenu);
+  }
+
+  render() {
+    return (
+      <TwoSideMenus
+        ref="menu"
+        leftMenu={<Menu ref="leftMenu" color="yellow"/>}
+        rightMenu={<Menu ref="rightMenu" color="red"/>}>
+        {this.props.children}
+      </TwoSideMenus>
+    );
+  }
+}
+
+class Intro extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Content color="black"/>
+    );
+  }
+}
+
+class Faq extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Content color="brown"/>
+    );
+  }
+}
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  async componentDidMount() {
+    const { scene } = this.refs;
+
+    await wait(2000);
+    scene.goto('/home/info', { side:'right', withAnimation: true });
+
+    await wait(2000);
+    scene.goto('/home/faq', { side:'right', withAnimation: true });
+  }
+
+  render() {
+    return (
+      <Scene ref="scene" initialPath="/login">
+        <Scene path="login" component={Login}/>
+        <Scene path="home" component={App} fresh={true}>
+          <Scene path="info" component={Intro}/>
+          <Scene path="faq" component={Faq}/>
+        </Scene>
+      </Scene>
+    );
+  }
+}
+
+AppRegistry.registerComponent('ExampleWith2SideMenus', () => Main);
