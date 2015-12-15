f31efe9b738405213e042ae286d9ccb9239110d5
Ali Najafizadeh <a.najafizadeh@gmail.com>
2015-12-15 17:12:57 -0500

    cleaned up code

---
+++ examples/Example2/app.js
@@ -0,0 +1,108 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Animated,
+  Component
+} = React;
+
+var window = Dimensions.get('window');
+
+import { Scene, SceneSide } from 'scene-router';
+
+const wait = (delay) => {
+  return new Promise((resolve, _) => {
+    setTimeout(resolve, delay)
+  });
+};
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  async componentDidMount() {
+    const scene = this.refs['scene'];
+    await wait(3000);
+    scene.goto("/scene/red", {}, { side: SceneSide.TOP });
+    await wait(3000);
+    scene.goto("/scene/yellow");
+    await wait(3000);
+    scene.goback();
+    await wait(3000);
+    scene.goback();
+    await wait(3000);
+    scene.goto("/scene/yellow", {}, { withAnimation: false });
+    await wait(3000);
+    scene.goback();
+  }
+
+  render() {
+    return (
+      <Scene ref="scene" initialPath="/scene/blue" initialProps={{}}>
+        <Scene path="scene/:color" component={CustomScene}></Scene>
+      </Scene>
+    );
+  }
+}
+
+class CustomScene extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  sceneWillFocus() {
+    const { params: { color } } = this.props;
+    console.log(`scene with color '${color}' will focus.`);
+  }
+
+  sceneDidFocus() {
+    const { params: { color } } = this.props;
+    console.log(`scene with color '${color}' did focus.`);
+  }
+
+  sceneWillBlur() {
+    const { params: { color } } = this.props;
+    console.log(`scene with color '${color}' will blur.`);
+  }
+
+  sceneDidBlur() {
+    const { params: { color } } = this.props;
+    console.log(`scene with color '${color}' did blur.`);
+  }
+
+  render() {
+    const { params: { color } } = this.props;
+    return (
+      <View style={[styles.view, { backgroundColor: color }]}>
+        <Text style={{top: 20 }} onPress={()=>console.log('hello')}>Press Me</Text>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    overflow: 'hidden',
+    position: 'absolute',
+    top:0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  view: {
+    position: 'absolute',
+    width: window.width,
+    height: window.height
+  }
+});
+
+module.exports = App;
--- examples/Example2/index.android.js
+++ examples/Example2/index.android.js
@@ -5,105 +5,10 @@
 'use strict';
 
 var React = require('react-native');
+var App = require('./app');
+
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Animated,
-  Component
 } = React;
 
-var window = Dimensions.get('window');
-
-import { Scene, SceneSide } from '../..';
-
-const wait = (delay) => {
-  return new Promise((resolve, _) => {
-    setTimeout(resolve, delay)
-  });
-};
-
-class Example2 extends Component {
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  async componentDidMount() {
-    const scene = this.refs['scene'];
-    await wait(3000);
-    scene.goto("/scene/red", {}, { side: SceneSide.TOP });
-    await wait(3000);
-    scene.goto("/scene/yellow");
-    await wait(3000);
-    scene.goback();
-    await wait(3000);
-    scene.goback();
-    await wait(3000);
-    scene.goto("/scene/yellow", {}, { withAnimation: false });
-    await wait(3000);
-    scene.goback();
-  }
-
-  render() {
-    return (
-      <Scene ref="scene" initialPath="/scene/blue" initialProps={{}}>
-        <Scene path="scene/:color" component={CustomScene}></Scene>
-      </Scene>
-    );
-  }
-}
-
-class CustomScene extends Component {
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  sceneWillFocus() {
-    const { params: { color } } = this.props;
-    console.log(`scene with color '${color}' will focus.`);
-  }
-
-  sceneDidFocus() {
-    const { params: { color } } = this.props;
-    console.log(`scene with color '${color}' did focus.`);
-  }
-
-  sceneWillBlur() {
-    const { params: { color } } = this.props;
-    console.log(`scene with color '${color}' will blur.`);
-  }
-
-  sceneDidBlur() {
-    const { params: { color } } = this.props;
-    console.log(`scene with color '${color}' did blur.`);
-  }
-
-  render() {
-    const { params: { color } } = this.props;
-    return (
-      <View style={[styles.view, { backgroundColor: color }]}>
-        <Text style={{top: 20 }} onPress={()=>console.log('hello')}>Press Me</Text>
-      </View>
-    );
-  }
-}
-
-var styles = StyleSheet.create({
-  container: {
-    overflow: 'hidden',
-    position: 'absolute',
-    top:0,
-    left: 0,
-    right: 0,
-    bottom: 0
-  },
-  view: {
-    position: 'absolute',
-    width: window.width,
-    height: window.height
-  }
-});
-
-AppRegistry.registerComponent('Example2', () => Example2);
+AppRegistry.registerComponent('Example2', () => App);
